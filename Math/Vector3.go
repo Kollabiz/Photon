ed30332a0190dca76a4d21dd8c08f4c6ac5bf624
Kollabiz <77539363+Kollabiz@users.noreply.github.com>
2024-09-23 19:17:29 +0300

    Implemented vectors

---
+++ Math/Vector3.go
@@ -0,0 +1,105 @@
+package Math
+
+import "math"
+
+type Vector3 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+// Vector constructors
+
+func NewVector3(x, y, z float64) Vector3 {
+	return Vector3{x, y, z}
+}
+
+func ZeroVector3() Vector3 {
+	return Vector3{0, 0, 0}
+}
+
+// Simple operations
+
+func (v Vector3) FMul(m float64) Vector3 {
+	return Vector3{v.X * m, v.Y * m, v.Z * m}
+}
+
+func (v Vector3) FDiv(m float64) Vector3 {
+	return Vector3{v.X / m, v.Y / m, v.Z / m}
+}
+
+func (v Vector3) IMul(m int) Vector3 {
+	return Vector3{v.X * float64(m), v.Y * float64(m), v.Z * float64(m)}
+}
+
+func (v Vector3) IDiv(m int) Vector3 {
+	return Vector3{v.X / float64(m), v.Y / float64(m), v.Z / float64(m)}
+}
+
+func (v Vector3) Abs() Vector3 {
+	return Vector3{
+		math.Abs(v.X),
+		math.Abs(v.Y),
+		math.Abs(v.Z),
+	}
+}
+
+// Vector operations
+
+func (v Vector3) LenSq() float64 {
+	return v.X*v.X + v.Y*v.Y + v.Z*v.Z
+}
+
+func (v Vector3) Len() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y + v.Z*v.Z)
+}
+
+func (v Vector3) Normalized() Vector3 {
+	l := v.Len()
+	return Vector3{
+		X: v.X / l,
+		Y: v.Y / l,
+		Z: v.Z / l,
+	}
+}
+
+func (v Vector3) Dot(o Vector3) float64 {
+	return v.X*o.X + v.Y*o.Y + v.Z*o.Z
+}
+
+func (v Vector3) Cross(o Vector3) Vector3 {
+	return Vector3{
+		X: v.Y*o.Z - v.Z*o.Y,
+		Y: v.Z*o.X - v.X*o.Z,
+		Z: v.X*o.Y - v.Y*o.X,
+	}
+}
+
+func (v Vector3) Inverse() Vector3 {
+	return Vector3{-v.X, -v.Y, -v.Z}
+}
+
+func (v Vector3) Add(o Vector3) Vector3 {
+	return Vector3{
+		X: v.X + o.X,
+		Y: v.Y + o.Y,
+		Z: v.Z + o.Z,
+	}
+}
+
+func (v Vector3) Sub(o Vector3) Vector3 {
+	return Vector3{
+		X: v.X - o.X,
+		Y: v.Y - o.Y,
+		Z: v.Z - o.Z,
+	}
+}
+
+// Comparing vectors
+
+func (v Vector3) Equal(o Vector3) bool {
+	if v.X == o.X && v.Y == o.Y && v.Z == o.Z {
+		return true
+	}
+	return false
+}
